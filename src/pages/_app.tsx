d7363d064b33a92f4e2d15b6ea1000d46c751ba9
keith-ssj <keithjara16@gmail.com>
2025-10-27 04:23:13 -0700

    Add src/pages/_app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app';
+import { LanguageProvider } from '../context/LanguageContext';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <LanguageProvider>
+      <Component {...pageProps} />
+    </LanguageProvider>
+  );
+}
+
+export default MyApp;
